9b48d4b955b105baf3121fa04641598cc2b9fb4b
Gustavo Oliveira <gustavo.gabriel@beresolution.com>
2022-01-06 15:23:48 -0300

    refactory error to utils

---
+++ src/utils/email-validator.spec.js
@@ -0,0 +1,27 @@
+const validator = require('validator')
+const EmailValidator = require('./email-validator')
+
+const makeSut = () => {
+  return new EmailValidator()
+}
+
+describe('Email validator', () => {
+  test('Should return true if validator returns true', () => {
+    const sut = makeSut()
+    const isEmailValid = sut.isValid('valid_email@email.com')
+    expect(isEmailValid).toBe(true)
+  })
+
+  test('Should return false if validator returns false', () => {
+    validator.isEmailValid = false
+    const sut = makeSut()
+    const isEmailValid = sut.isValid('valid_email')
+    expect(isEmailValid).toBe(false)
+  })
+
+  test('Should call validatorEmail with correct email', () => {
+    const sut = makeSut()
+    sut.isValid('valid_email')
+    expect(validator.email).toBe('valid_email')
+  })
+})
---
+++ src/utils/errors/index.js
@@ -0,0 +1,7 @@
+const MissingParamError = require('./missing-param-error')
+const InvalidParamError = require('./invalid-param-erros')
+
+module.exports = {
+  MissingParamError,
+  InvalidParamError
+}
