83f45dc94cac8f7817863949ea302efea302b13a
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2023-12-15 10:23:33 -0300

    chore: add jest to lint-staged

---
+++ jest.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  clearMocks: true,
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  coverageProvider: 'v8',
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest'
+  }
+}
+
+export default config
---
+++ src/test.spec.ts
@@ -0,0 +1,5 @@
+describe('Teste', () => {
+  it('should be true', () => {
+    expect(1).toBe(1)
+  })
+})
