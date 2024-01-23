7b63ded9613e86ef749790e9aaeea5e4c91845d0
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2024-01-23 09:17:17 -0300

    test: emsire request parse body as json

---
+++ src/main/middlewares/body-parser.test.ts
@@ -0,0 +1,14 @@
+import request from 'supertest'
+
+import { app } from '../config/app'
+
+describe('Body Parser Middleware', () => {
+  it('should parse body as json', async () => {
+    app.post('/test_body_parser', (request, response) => {
+      response.send(request.body)
+    })
+
+    const response = await request(app).post('/test_body_parser').send({ name: 'Gustavo Oliveira' })
+    expect(response.body).toEqual({ name: 'Gustavo Oliveira' })
+  })
+})
