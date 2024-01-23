92eec4c6e744774aabc2891b3040a524f1ddf6c9
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2024-01-23 10:30:32 -0300

    refactor: connect to mongodb before integration tests

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,10 +1,25 @@
 import request from 'supertest'
 
 import { app } from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 // testar somente casos de sucesso
 
 describe('SignUp Routes', () => {
+  beforeAll(async () => {
+    // eslint-disable-next-line @typescript-eslint/non-nullable-type-assertion-style
+    await MongoHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   it('should return an account on success', async () => {
     await request(app).post('/api/signup')
       .send({
