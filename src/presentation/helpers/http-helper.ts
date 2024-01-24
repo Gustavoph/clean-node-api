aaf9238490cc1dcf73a68606349bd7c5e0417c1f
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2024-01-24 10:36:56 -0300

    feat: ensure LogControllerDecorator calls LogErrorRepository if controller returns a server error

--- src/presentation/helpers/http-helper.ts
+++ src/presentation/helpers/http-helper.ts
@@ -7,7 +7,7 @@ export const badRequest = (error: Error): HttpResponse => ({
 })
 
 export const serverError = (error: Error): HttpResponse => ({
-  statusCode: 400,
+  statusCode: 500,
   body: new ServerError(error.stack!)
 })
 
