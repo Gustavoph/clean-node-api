c51c9a98527929efdb843e317469b53a090f0af2
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2024-01-25 10:40:14 -0300

    feat: add docker on project

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,4 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://localhost:27017/clean-node-api',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://docker:docker@localhost:27017/?authMechanism=DEFAULT',
   port: process.env.PORT || 5050
 }
