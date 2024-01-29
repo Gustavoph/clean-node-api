d27c970f2578d66db0f2eee8b99e092d9b342741
gustavoph <gusta.coliveira07@gmail.com>
2024-01-29 10:08:39 -0300

    refactor: remove duplicated code

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -7,18 +7,11 @@ export class LoginController implements Controller {
   constructor (private readonly emailValidator: EmailValidator) {}
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
-    }
-    if (!httpRequest.body.password) {
-      return badRequest(new MissingParamError('password'))
-    }
-    const isValid = this.emailValidator.isValid(httpRequest.body.email)
-
-    if (!isValid) {
-      return badRequest(new InvalidParamError('email'))
-    }
-
+    const { email, password } = httpRequest.body
+    if (!email) { return badRequest(new MissingParamError('email')) }
+    if (!password) { return badRequest(new MissingParamError('password')) }
+    const isValid = this.emailValidator.isValid(email)
+    if (!isValid) { return badRequest(new InvalidParamError('email')) }
     return ok({})
   }
 }
