3fb3a502217fc8c96d6e0fdf5feb0f722f5024c4
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2024-01-31 09:16:48 -0300

    test: ensure ValidationComposite return an error if any validation fails

---
+++ src/presentation/helpers/validators/validation-composite.spec.ts
@@ -0,0 +1,33 @@
+import { MissingParamError } from '../../errors'
+import { type Validation } from './validation'
+import { ValidationComposite } from './validation-composite'
+
+const makeValidation = (): Validation => {
+  class ValidationStub implements Validation {
+    validate (input: any): Error | null {
+      return null
+    }
+  }
+
+  return new ValidationStub()
+}
+
+interface SutTypes {
+  validationStub: Validation
+  sut: ValidationComposite
+}
+
+const makeSut = (): SutTypes => {
+  const validationStub = makeValidation()
+  const sut = new ValidationComposite([validationStub])
+  return { sut, validationStub }
+}
+
+describe('Validation Composite', () => {
+  it('should return an error if any validation fails', () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate').mockReturnValue(new MissingParamError('field'))
+    const error = sut.validate({ field: 'any_value' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})
