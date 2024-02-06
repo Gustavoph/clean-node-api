3092e19cca388aa02c132d614039f56e720cc659
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2024-02-06 10:07:04 -0300

    feat: add LoginFactory

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,4 +1,6 @@
 export default {
   mongoUrl: process.env.MONGO_URL || 'mongodb://docker:docker@localhost:27017/?authMechanism=DEFAULT',
-  port: process.env.PORT || 5050
+  port: process.env.PORT || 5050,
+  jwtSecret: process.env.JWT_SECRET || '53cr3t',
+  salt: process.env.SALT || 12
 }
