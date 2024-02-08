e7974916621a07566f1e3990d6fdb226b8d0aeef
Gustavo Oliveira <gustavo.gabriel@beresolution.com.br>
2024-02-08 09:41:05 -0300

    chore: add docker compose

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
-  mongoUrl: process.env.MONGO_URL || 'mongodb://docker:docker@localhost:27017/?authMechanism=DEFAULT',
+  mongoUrl: process.env.MONGO_URL || 'mongodb://docker:docker@mongo:27017/?authMechanism=DEFAULT',
   port: process.env.PORT || 5050,
   jwtSecret: process.env.JWT_SECRET || '53cr3t',
   salt: process.env.SALT || 12
